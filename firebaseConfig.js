39bb858936e25b510cca3b28392a1e4b84181aaf
fncyz <eurielvinz0718@gmail.com>
2025-08-27 09:46:37 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuY0sUdObl18_WQMUHF5j3iSi1dFsUxZU",
+  authDomain: "egdf-goals.firebaseapp.com",
+  projectId: "egdf-goals",
+  storageBucket: "egdf-goals.firebasestorage.app",
+  messagingSenderId: "832747760170",
+  appId: "1:832747760170:web:da979b4e6a6899bf9df153"
+};
+
+const app = initializeApp(firebaseConfig);
